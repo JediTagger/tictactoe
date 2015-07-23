cd480970becbb4e576447f1e5511312b9ffb9e36
AustinScott <Ascott3573@gmail.com>
2015-07-23 14:43:48 -0500

    Third commit, toggle sorta works

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
 //****************************************
 // Click event for a div
 //****************************************
-$('div').click(function () {
-  var $allDivs = $('div');
-  $allDivs.css('border', 'none');
-  $allDivs.removeClass('selectedDiv');
+// $('td').click(function () {
+//   var $allDivs = $('td');
+//   $allDivs.css('border', 'none');
+//   $allDivs.removeClass('selectedDiv');
   
-  var $clickedDiv = $(this);
-  $clickedDiv.css('border', '1px solid black');
-  $clickedDiv.addClass('selectedDiv');
+//   var $clickedDiv = $(this);
+//   $clickedDiv.css('border', '1px solid black');
+//   $clickedDiv.addClass('selectedDiv');
   
-  $('form').removeClass('hidden');
-});
+//   $('form').removeClass('hidden');
+// });
 
 //****************************************
 // Click event for the form button
 //****************************************
-$('button').click(function(event) {
-  event.preventDefault();
+// $('button').click(function(event) {
+//   event.preventDefault();
   
-  var color = $('input').val();
+//   var color = $('input').val();
   
-  $('.selectedDiv').css('background', color);
+//   $('.selectedDiv').css('background', color);
    
+// });
+
+// $('td').click(function () {
+//   $('this').css("background-color", 'blue');
+
+// }
+
+$( "td" ).onClick(function() {
+  $( this ).toggleClass( "border blue");
 });
